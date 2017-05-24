ed85ec4b72413737b5a12b0416380337eb129ced
ZJH <799243917@qq.com>
2017-05-24 12:39:21 +0800

    完成根据roomid获取照片的名称，json数组

--- SSHOME/src/cn/zowzy/action/FileAction.java
+++ SSHOME/src/cn/zowzy/action/FileAction.java
@@ -3,8 +3,15 @@ package cn.zowzy.action;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.struts2.ServletActionContext;
 
 import com.opensymphony.xwork2.ActionSupport;
+
+import cn.zowzy.entity.Photo;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 /**
  * 
@@ -53,7 +60,8 @@ public class FileAction extends ActionSupport {
 		}
 
 		// 创建目标文件夹
-		String photoPath = "D:/roomImages/" + roomid + "/";
+		String photoPath = ServletActionContext.getRequest().getRealPath("/") + "roomImages" + File.separator + roomid
+				+ File.separator;
 		File f = new File(photoPath);
 		if (!f.exists()) {
 			f.mkdirs();
@@ -62,6 +70,7 @@ public class FileAction extends ActionSupport {
 			try {
 				// FileUtils.copyFile(file, new FileOutputStream(new
 				// File(photoPath + name)));
+
 				FileInputStream fis = new FileInputStream(file);
 				FileOutputStream fos = new FileOutputStream(new File(photoPath + name));
 				byte[] bytes = new byte[1024];
@@ -72,6 +81,11 @@ public class FileAction extends ActionSupport {
 				}
 				fis.close();
 				fos.close();
+
+				File files = new File(photoPath);
+				String[] list = files.list();
+				System.out.println("当前目录下的照片数目为：" + list.length);
+
 				return SUCCESS;
 			} catch (Exception e) {
 				System.out.println("照片接受失败");
@@ -85,6 +99,48 @@ public class FileAction extends ActionSupport {
 
 	public String photoUploadPage() {
 		return SUCCESS;
+	}
+
+	public String getPhotosPage() {
+		return SUCCESS;
+	}
+
+	/**
+	 * 
+	 * <p>MethodName: getPhotoNames</p>
+	 * <p>Description:根据房源编号获取这个房源的照片,如果没有返回的是空JSON数组</p> 
+	 * @author ZJH
+	 * @throws UnsupportedEncodingException 
+	 * @date 2017年5月24日上午11:44:55
+	 */
+	public void getPhotoNames() throws Exception {
+		String photoNames;
+		if (roomid == null || roomid.length() <= 0) {
+			JSONArray array = new JSONArray();
+			photoNames = array.toString();
+			ServletActionContext.getResponse().getOutputStream().write(photoNames.getBytes("utf-8"));
+		} else {
+			String photoPath = ServletActionContext.getRequest().getRealPath("/") + "roomImages" + File.separator
+					+ roomid + File.separator;
+			File f = new File(photoPath);
+			if (!f.exists()) {
+				f.mkdirs();
+			}
+			File files = new File(photoPath);
+			String[] list = files.list();
+			System.out.println("当前目录下的照片数目为：" + list.length);
+			JSONArray array = new JSONArray();
+			for (String string : list) {
+				Photo p = new Photo();
+				p.setRoomid(roomid);
+				p.setFilename(string);
+				JSONObject obj = JSONObject.fromObject(p);
+				array.add(obj);
+			}
+			photoNames = array.toString();
+			ServletActionContext.getResponse().getOutputStream().write(photoNames.getBytes("utf-8"));
+		}
+		System.out.println("用户请求照片：" + photoNames);
 	}
 
 }
